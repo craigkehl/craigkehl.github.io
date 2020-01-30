de5e447457779e05566029957ce6ea4f5bc1af50
craigkehl <keh19003@byui.edu>
2020-01-29 17:51:57 -0700

    Lesson 4 submint

---
+++ lesson4/js/main.js
@@ -0,0 +1,20 @@
+const hambutton = document.querySelector(".hamburger");
+
+var lastModification = new Date(document.lastModified);
+
+// initialize custom date arrays
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+/* Assemble date output string*/
+let lastModifiedDate = days[lastModification.getDay()] + ", " + lastModification.getDate() + " " + months[lastModification.getMonth()] + " " + lastModification.getFullYear();
+
+
+document.getElementById("lastMod").innerHTML=lastModifiedDate;
+
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+document.querySelector(".navigation").classList.toggle("responsive");
+}
