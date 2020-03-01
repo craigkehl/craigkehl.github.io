def88f768ce70dcc1f71212ab7fb6349b51bc09d
craigkehl <keh19003@byui.edu>
2020-02-29 23:54:03 -0700

    form push

---
+++ lesson8/app.js
@@ -0,0 +1,38 @@
+
+// Progressive loading images
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+// Object containing parameters being set for the IntersectionObserver
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -50px 0px"
+};
+
+const loadImages = (image) => {
+	image.setAttribute('src', image.getAttribute('data-src'));
+	image.onload = () => {
+		image.removeAttribute('data-src');
+	};
+};
+
+// check to see if Intersection Ovserver is supported
+if('IntersectionObserver' in window) {
+	const imgObserver = new IntersectionObserver((items, observer) => {
+		items.forEach((item) => {
+			if(item.isIntersecting) {
+				loadImages(item.target);
+				observer.unobserve(item.target);
+			}
+		});
+  }, imgOptions);
+  
+  // Loop through each img an check status and load if necsssary
+	imagesToLoad.forEach((img) => {
+		imgObserver.observe(img);
+	});
+}
+else {
+	imagesToLoad.forEach((img) => {
+		loadImages(img);
+	});
+}
