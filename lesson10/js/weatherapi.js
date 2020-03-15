9ec92f5e483dfa354923f467a17972992e9e90bb
craigkehl <keh19003@byui.edu>
2020-03-14 22:49:23 -0600

    4

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,9 @@
 const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=f7dad340bc2e67bd09fa4951917cee86';
 var cTmp;
 var wSpeed;
+var desc;
+var humidty;
+var imagesrc;
 
 fetch(apiURL)
   .then((response) => response.json())
@@ -9,14 +12,14 @@ fetch(apiURL)
     console.log(wSpeed);
     document.getElementById('current-temp').textContent = Math.round(((jsObject.main.temp - 273.15) * 9/5 + 32) * 100) / 100;
     wSpeed = jsObject.wind.speed;
-    const humidity = jsObject.main.humidity;
+    humidty = jsObject.main.humidty;
     
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
-    const desc = jsObject.weather[0].description;
+    imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+    desc = jsObject.weather[0].description;
     document.getElementById('imagesrc').textContent = imagesrc; // display image source in paragraph
     document.getElementById('icon').setAttribute('src', imagesrc); // set the icon source 
     document.getElementById('icon').setAttribute('alt', desc);
-    document.getElementById("humidty").textContent = humidity;
+    document.getElementById("humidty").textContent = humidty;
     document.getElementById('currently').textContent = desc;
     // Windchill
     
