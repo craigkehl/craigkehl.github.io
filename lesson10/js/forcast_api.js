66cee15cb9094f7fe1bda5f481fa58ec3477e2e5
craigkehl <keh19003@byui.edu>
2020-03-15 23:38:32 -0600

    5 day forcast in

--- lesson10/js/forcast_api.js
+++ lesson10/js/forcast_api.js
@@ -1,27 +1,41 @@
 
 
 const forcastURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=f7dad340bc2e67bd09fa4951917cee86';
+var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; 
 
 fetch(forcastURL)
   .then((rsponse) => rsponse.json())
   .then((jsnObject) => {
+    const forcastUTC = jsnObject['list'];
+    let c = 1;
+    forcastUTC.filter(row=>{
+      return fiveDay().includes(row.dt * 1000)
+    }).forEach(row => {
+      var d = new Date(row.dt_txt); 
+      document.getElementById('day' + c + '_label').textContent = days[d. getDay()];
+      let temp = Math.round((((row.main.temp - 273.15) * 9/5 + 32) * 100) / 100);
+      document.getElementById('day' + c + '_icon').setAttribute('src', `https://openweathermap.org/img/w/${row.weather[0].icon}.png`);
+      document.getElementById('day' + c + '_temp').textContent = (temp + "\xB0 F");
+      c++;
+    })
     console.log(jsnObject);
 
   });
 
 
+// Get UTC Timestamps for next 5 days
+function fiveDay() {
+  rightNow = new Date();// get current date
+  let fiveDayUTC = new Array(5);
+  sixPm = new Date(rightNow.setHours(18,0,0,0)); // forcast for tonight
+  fiveDayUTC[0] = (rightNow.setHours(18,0,0,0));
 
+  for (let i = 1; i < 6; i++) {
+    fiveDayUTC[i] = (rightNow.setHours(18,0,0,0) + ((24 * 60 * 60 * 1000) * i)); 
+  }
+  return fiveDayUTC;
+}  
+  
 
-  // Copy & Paste this
-Date.prototype.getUnixTime = function() { return this.getTime()/1000|0 };
-if(!Date.now) Date.now = function() { return new Date(); }
-Date.time = function() { return Date.now().getUnixTime(); }
-
-// Get the current time as Unix time
-var currentUnixTime = Date.time();
-currentUnixTime = Date.now().getUnixTime(); // same as above
-
-// Parse a date and get it as Unix time
-var parsedTime = new Date('Sun, 15 Mar 2020 18:00:00 MST').getUTCMilliseconds();
 
 
