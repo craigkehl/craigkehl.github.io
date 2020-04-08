5b42fc543259031b8b5390dfe0db1d45238e8930
craigkehl <keh19003@byui.edu>
2020-04-08 12:59:34 -0600

    added click2call

--- Salmon_River_Adv/js/sticky_nav.js
+++ Salmon_River_Adv/js/sticky_nav.js
@@ -3,15 +3,17 @@ window.onscroll = function() {myFunction()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
-
+var call_icon = document.getElementById("call_icon");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
+    call_icon.setAttribute("class", "sticky2");
   } else {
     navbar.classList.remove("sticky");
+    call_icon.setAttribute("class", "non-sticky2");
   }
 }
