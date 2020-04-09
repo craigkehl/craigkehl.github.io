0c17ed63e68e3d9f5db2ad22fb869236d514a1af
craigkehl <keh19003@byui.edu>
2020-04-09 00:26:19 -0600

    fix messups

--- Salmon_River_Adv/js/trips.js
+++ Salmon_River_Adv/js/trips.js
@@ -5,34 +5,34 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    const guides = jsonObject['guides'];
+    const guides = jsonObject['trips'];
     console.log(jsonObject);
-    guides.forEach(guide => {
+    guides.forEach(trip => {
       let card = document.createElement('section');
       let image = document.createElement('img');
       let divInfo = document.createElement('div');
       let gYears = document.createElement('span');
-      let gHome = document.createElement('span');
-      let gEduc = document.createElement('span');
+      let tLength = document.createElement('span');
+      let tDates = document.createElement('span');
       let tName = document.createElement('h3');
-      let tMotto = document.createElement('h4');
+      let tCost = document.createElement('h4');
 
 
-      tName.textContent = guide.firstname;
-      tMotto.textContent = `MY MOTTO:\n"${guide.motto}"`;
-      gYears.textContent = `YEARS EXPERIENCE: ${guide.yrsexperience}`; //complete Years of Experience
-      gHome.textContent = `HOMETOWN: ${guide.hometown}`;  // complete string
-      gEduc.textContent = `EDUCATION: ${guide.education}`;   // complete string Education background
-      image.setAttribute ('src', guide.photo);
-      image.setAttribute('alt', `Picture of ${guide.firstname} ${guide.lastname}`);
+      tName.textContent = trip.name;
+      tCost.textContent = `COST:"${trip.cost}"`;
+      gYears.textContent = `DIFFICULTY: ${trip.Difficulty}`;
+      tLength.textContent = `LENGTH: ${trip.Length}`;  
+      tDates.textContent = `DATES: ${trip.Dates}`;
+      image.setAttribute ('src', trip.photo);
+      image.setAttribute('alt', `Picture of ${trip.name}`);
 
       divInfo.appendChild(gYears);
-      divInfo.appendChild(gHome);
-      divInfo.appendChild(gEduc);
+      divInfo.appendChild(tLength);
+      divInfo.appendChild(tDates);
       card.appendChild(image);
       card.appendChild(divInfo);
       card.appendChild(tName);
-      card.appendChild(tMotto);
+      card.appendChild(tCost);
 
       document.querySelector('div.trips_page').appendChild(card);     
 
